c78be7027f99577f92bd8c286a16aefd337be59c
zytian9 <34347548+zytian9@users.noreply.github.com>
2023-02-10 14:17:12 -0500

    add new modes

--- code/classify.py
+++ code/classify.py
@@ -23,38 +23,48 @@ from feature_selection_template import *
 
 
 # Feb 6th: commented one line for reading the existing data by seeds, see line 56
-# Feb 6th: added gridsearch and classfication report
-# Feb 10th: added default to use pickled data
-
+# Feb 6th: added gridsearch and classfication report\
+# Feb 10th: update training and testing data
+# Feb 10th: add two modes: train on 1 test on 4, and train on 4 test on 1
+# ex1: python classify.py -d bf -e fe -m merge -f word -r 1,3
+# ex2: python classify.py -d bf -e fe -m one -f word -r 1,3
 
 def main():
-    if len(sys.argv) != 9:
+    if len(sys.argv) < 10:
+        print (len(sys.argv))
         usage(sys.argv[0])
         exit(1)
 
-    data_index, train_perc, test_perc, feat, n1, n2 = process_args(sys.argv[0])
+    seed, seed_eval, mode, feat, n1, n2 = process_args(sys.argv[0])
 
-#    # check to make sure the LOCO partition exists
-#    if not os.path.exists('../data/LOCO_partition.json'):
-#        partition_dataset()
-#
-#    # open the partitioned data
-#    with open('../data/LOCO_partition.json') as f:
-#        data = json.load(f)
-#
-#    # get the data (i realize this is inefficient)
-#    ct = split_data(data)
-#    ct = ct[data_index]
-#
-#    # split the data into x and y vectors
-#    xdata, ydata = x_y_split(ct)
-#
-#    # split the data into train and test
-#    xtrain, xtest, ytrain, ytest = train_test_split(xdata, ydata,
-#                                                    train_size = train_perc,
-#                                                    test_size = test_perc)
+    print (seed)
+    print (seed_eval)
 
-    xtrain, xtest, ytrain, ytest  = read_data(index_to_seed(data_index))
+    # check to make sure the LOCO partition exists
+    # if not os.path.exists('../data/LOCO_partition.json'):
+    #     partition_dataset()
+
+    # open the partitioned data
+    # with open('../data/LOCO_partition.json') as f:
+    #     data = json.load(f)
+
+    # get the data (i realize this is inefficient)
+    # ct = split_data(data)
+    # ct = ct[data_index]
+
+    # split the data into x and y vectors
+    # xdata, ydata = x_y_split(ct)
+
+    # # split the data into train and test
+    # xtrain, xtest, ytrain, ytest = train_test_split(xdata, ydata,
+    #                                                 train_size = train_perc,
+    #                                                 test_size = test_perc)
+    
+    if mode == 'merge':
+        # test on seed, train on a list without seed
+        xtrain, ytrain, xtest,  ytest = read_data_merge(seed,seed_eval)
+    else:
+        xtrain, ytrain, xtest, ytest  = read_data(seed,seed_eval)
 
     # vectorize the data
     vectorizer = CountVectorizer(analyzer = feat, ngram_range = (n1, n2))
@@ -64,24 +74,25 @@ def main():
     vec_xtest = vectorizer.transform(xtest)
 
     # create SVM model and grid search
-    svm = LinearSVC()
+    svc = LinearSVC()
     param_grid = {'loss': ['squared_hinge'],
                       'random_state': [1291],
                       'C': [0.01,0.1,1]}
 
-    model = GridSearchCV(svm, param_grid, n_jobs=16, verbose=1, cv=5)
+    model = GridSearchCV(svc, param_grid, n_jobs=16, verbose=1, cv=5)            
 
     # print out our model parameters
-    print(svm.get_params)
+    # print(model.get_params)
 
     # fit data
-    svm.fit(vec_xtrain, ytrain)
+    model.fit(vec_xtrain, ytrain)
 
     # predict with our svm model
-    preds = svm.predict(vec_xtest)
+    preds = model.predict(vec_xtest)
 
     # check how our model did
-    print(classification_report(ytest, preds, digits=4))
+    print("SVM accuracy: ", accuracy_score(preds, ytest) * 100)
+    print (classification_report(ytest, preds, digits=4))
 
 
 if __name__ == "__main__":
