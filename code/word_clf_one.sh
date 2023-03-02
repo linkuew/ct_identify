5c382681b2b9bfa5ffee8ac1c5faf1915589bcb8
zytian9 <34347548+zytian9@users.noreply.github.com>
2023-03-02 11:51:31 -0500

    Update word_clf_one.sh

--- code/word_clf_one.sh
+++ code/word_clf_one.sh
@@ -1,5 +1,5 @@
 for data in bf fe cc va pg; do  
   for e in bf fe cc va pg; do  
-      python3 classify.py -d bf -e fe -m one -f word -r 1,3
+      python3 classify.py -d $data -e $e -m one -f word -r 1,3
   done
 done
