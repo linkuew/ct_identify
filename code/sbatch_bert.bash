07316a067dd359c9528197e9ad9f3568a2530403
Matthew <37814869+linkuew@users.noreply.github.com>
2023-03-02 11:33:09 -0500

    Adds some fixes to the bash file to make it user agnostic and iterate

--- code/sbatch_bert.bash
+++ code/sbatch_bert.bash
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-=======
-#SBATCH --account r00018
->>>>>>> 208ab7423b54c6b954791fb8c1dcdd0a086abf80
-#SBATCH -p dl
+#SBATCH --mail-user=PLACES USER EMAIL ADDRESS HERE
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node p100:1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-30:00:00
+#SBATCH --mem=50gb
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --job-name=ct_flf
 #SBATCH -o base_out_%j.out
 #SBATCH -e base_error_%j.err
-#SBATCH --mail-user=zuoytian@iu.edu
-#SBATCH --mail-type=ALL
-#SBATCH -J ct_clf
-#SBATCH --time=30:00:00
 
-for data in bf; do  
-  for e in fe; do  
-    for p in 5; do 
-        python bert_clf.py -d $data -e $e -m one -p 1 -l 1e-5 -b 5
-    done
-  done
+## This is necessary for some, uncomment if you need it
+# module load python
+
+# testing array with the data
+declare -a cts=("bf" "fe" "cc" "va" "pg")
+
+# This is our training set
+tr="pg"
+
+# iterate through each CT, train on the same one but test on all the others
+for te in "${cts[@]}"; do
+        if [ "$tr" != "$te" ]; then
+                python bert_clf.py -d $tr -e $te -m one -p 1 -l 1e-5 -b 5
+        fi
 done
- 
+
 
