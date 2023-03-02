5a528cb304d4dea9dfe72cd6ec14428b9f35da3d
zytian9 <34347548+zytian9@users.noreply.github.com>
2023-03-02 11:52:30 -0500

    Update char_clf_one.sh

--- code/char_clf_one.sh
+++ code/char_clf_one.sh
@@ -1,5 +1,5 @@
 for data in bf fe cc va pg; do  
   for e in bf fe cc va pg; do  
-      python3 classify.py -d bf -e fe -m one -f char -r 2,7
+      python3 classify.py -d $data -e $e -m one -f char -r 2,7
   done
 done
