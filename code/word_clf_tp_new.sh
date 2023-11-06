830fc0bc31441140398618880338d07364ef4480
zytian9 <34347548+zytian9@users.noreply.github.com>
2023-11-06 14:32:44 -0500

    Update word_clf_tp_new.sh

--- code/word_clf_tp_new.sh
+++ code/word_clf_tp_new.sh
@@ -13,12 +13,12 @@ for i in 10 15 20; do
             echo "Evaluating: $e"
 
             # Run the topic_word_bleach.py script
-            python3 topic_word_bleach.py --n_topics $i
+            # python3 topic_word_bleach.py --n_topics $i
             # Check if the script executed successfully
-            if [ $? -ne 0 ]; then
-                echo "Error during topic_word_bleach for n_topics=$i, data=$data, e=$e"
-                exit 1
-            fi
+            # if [ $? -ne 0 ]; then
+            #    echo "Error during topic_word_bleach for n_topics=$i, data=$data, e=$e"
+            #    exit 1
+            # fi
 
             # Run the classify.py script
             python3 classify.py -d $data -e $e -m one -f tp$i -r 1,3 -o ./result_tp$i/
